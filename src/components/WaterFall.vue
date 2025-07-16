7dd88299b5cf8ffecd3c4eb12d6f0f6dc7792011
guanglian.bai <guanglian.bai@bangtu-ai.com>
2025-07-16 11:27:57 +0800

    feat: css 实现瀑布流

---
+++ src/components/WaterFall.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1>CSS 实现瀑布流</h1>
+  <div class="container">
+    <div class="item" v-for="item in data" :key="item.id">
+      <img :src="item.sources['1x']" alt="" />
+    </div>
+  </div>
+</template>
+<script lang="ts" setup>
+import data from "./data.json";
+</script>
+<style scoped lang="scss">
+.container {
+  column-gap: 0;
+  column-count: 4;
+}
+.item {
+  padding: 2px;
+  position: relative;
+  counter-increment: item-counter;
+}
+.item img {
+  display: block;
+  width: 100%;
+  height: auto;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="home">
     <h1>Home</h1>
+    <water-fall />
   </div>
 </template>
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import WaterFall from "../components/WaterFall.vue";
+</script>
 <style scoped lang="scss"></style>
