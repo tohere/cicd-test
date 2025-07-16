c1ad2022c18f09e3aca00801eb8bbb1176db9ac2
guanglian.bai <guanglian.bai@bangtu-ai.com>
2025-07-16 16:52:55 +0800

    pref: 优化布局

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="container">
-    <router-link to="/">首页</router-link>
-    <router-link to="/about">关于</router-link>
+  <div class="wrap">
+    <div class="container">
+      <router-link to="/">首页</router-link>
+      <router-link to="/about">关于</router-link>
+    </div>
+    <div class="content">
+      <router-view />
+    </div>
   </div>
-  <router-view />
 </template>
 
 <style scoped lang="scss">
@@ -16,6 +20,19 @@
   gap: 10px;
   padding: 20px 0;
   font-size: 24px;
+  flex-shrink: 0;
+}
+.wrap {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+.content {
+  flex: 1;
+  height: 0;
+  overflow: auto;
+  padding: 20px;
 }
 </style>
 
